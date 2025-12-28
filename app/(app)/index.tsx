c7cf876b9d7b063d05377b34b5b0a0489090d47d
Glenardd <tolentinojanglenard5@gmail.com>
2025-12-29 04:26:56 +0800

    added (app)/index

---
+++ app/(app)/index.tsx
@@ -0,0 +1,76 @@
+import SignOutButton from "@/components/social-auth-buttons/sign-out-button";
+import { Users } from "@/data/mockData";
+import {
+    Alert,
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+
+export default function Home() {
+    const data_ = new Users();
+    const userData = data_.getData();
+    
+    return (
+        // return list
+        <ScrollView
+            style={{
+                backgroundColor: "#070C27"
+            }}
+            contentContainerStyle={{
+                gap: 20,
+                padding: 30
+            }}
+        >
+            {userData.map((user_) => {
+
+                const searchData = data_.findData(user_.id)
+                const id_ = searchData?.id;
+                const projectName_ = searchData?.projectName;
+                const activities_ = searchData?.activties;
+
+                return (
+                    <Pressable
+                        key={user_.id}
+                        onPress={() => {
+                            Alert.alert('Example only',
+                                `id: ${id_}
+                                \nName: ${projectName_}
+                                \nActivities: ${activities_?.map(activity =>`${JSON.stringify(activity.activityName)}`)}
+                                `, [
+                                {
+                                    text: "Exit",
+                                    style: "cancel"
+                                },
+                            ]);
+                        }}
+                    >
+                        <View
+                            style={{
+                                backgroundColor: "#172038",
+                                padding: 10,
+                                borderRadius: 10,
+                                borderColor: "#625B71",
+                                borderWidth: 1.5,
+                            }}
+                        >
+                            <Text style={text.white}>
+                                {user_.projectName}
+                            </Text>
+                        </View>
+                    </Pressable>
+                )
+            })}
+            <SignOutButton />
+        </ScrollView>
+    )
+}
+
+//text color
+const text = StyleSheet.create({
+    white: {
+        color: "white"
+    }
+})
