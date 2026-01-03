4a4cc88bfe675b4191f778db00d1f3cf630721f1
Glenardd <tolentinojanglenard5@gmail.com>
2026-01-03 20:41:17 +0800

    change bg of the project library

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,87 +1,11 @@
+import LogoTitle from "@/components/icon-sceen-header";
 import ScreenHeader from "@/components/screen-header";
-import SignOutButton from "@/components/social-auth-buttons/sign-out-button";
-import { SplashScreenController } from '@/components/splash-screen-controller';
+import { SplashScreenController } from "@/components/splash-screen-controller";
 import { useAuthContext } from '@/hooks/use-auth-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LinearGradient } from "expo-linear-gradient";
 import { Redirect } from 'expo-router';
-import { useState } from 'react';
-import {
-  Dimensions,
-  Image,
-  Modal,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
 import Home from ".";
 import Forms from "./forms";
-
-interface User {
-  image_url: string
-  username: string
-}
-// header contents
-function LogoTitle({ image_url, username }: User) {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  return (
-    <View style={header.row_lg}>
-      <View style={[header.row_sm]}>
-        <LinearGradient
-          colors={["#63D0FF", "#427CE8", "#235691"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-          style={{
-            width: 55,
-            height: 55,
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}>
-          <Image
-            source={require("../../assets/images/projaxis.png")}
-            style={{
-              width: 50,
-              height: 50,
-            }}
-            resizeMode="contain"
-          />
-        </LinearGradient>
-        <View style={header.col}>
-          <Text style={text.head}>Projaxis</Text>
-          <Text style={text.secondHead}>Project Library</Text>
-        </View>
-        <Modal
-          visible={modalVisible}
-          transparent
-          animationType="slide"
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <TouchableOpacity
-            style={menu.overlay}
-            activeOpacity={1}
-            onPress={() => setModalVisible(false)}
-          >
-            <View style={menu.menuContainer}>
-              <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-                <Image source={{ uri: image_url }} style={{ height: 50, width: 50, borderRadius: 50 }} />
-                <Text style={{ fontSize: 14, fontWeight: "600" }}>{username}</Text>
-              </View>
-              <SignOutButton />
-            </View>
-          </TouchableOpacity>
-        </Modal>
-      </View>
-      <TouchableOpacity
-        onPress={() => setModalVisible(true)}
-      >
-        <Image source={{ uri: image_url }} style={{ height: 50, width: 50, borderRadius: 50 }} />
-      </TouchableOpacity>
-    </View >
-  )
-}
 
 export default function HomeLayout() {
   const { session, isLoading } = useAuthContext()
@@ -110,11 +34,12 @@ export default function HomeLayout() {
         headerTitleStyle: {
           fontWeight: 'bold',
         },
+        contentStyle: {backgroundColor: "#070C27"}
       }} />
       <Stack.Screen name="forms/index" component={Forms} options={{
         headerShown: true,
         headerShadowVisible: false,
-        headerTitle: () => <ScreenHeader title="Add Activity" subtitle="Add new activity to the current project"/>,
+        headerTitle: () => <ScreenHeader title="Add Project" subtitle="Add new project" currentPage="Project Library"/>,
         headerStyle: {
           backgroundColor: "#070C27"
         },
@@ -124,63 +49,8 @@ export default function HomeLayout() {
         },
         headerLeft: () => null,
         headerBackVisible:false,
+        contentStyle: {backgroundColor: "#070C27"}
       }} />
     </Stack.Navigator>
   )
 }
-
-//nav-header
-const header = StyleSheet.create({
-  row_sm: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10
-  },
-  row_lg: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingBottom: 15,
-    paddingHorizontal: 15,
-  },
-  col: {
-    flexDirection: "column",
-    alignItems: "center",
-  }
-});
-
-
-const text = StyleSheet.create({
-  head: {
-    fontSize: 30,
-    color: "white"
-  },
-  secondHead: {
-    fontSize: 15,
-    color: "#AEB7DA"
-  }
-});
-
-const menu = StyleSheet.create({
-  overlay: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.4)",
-    justifyContent: "flex-end", // pushes modal to bottom
-  },
-
-  menuContainer: {
-    height: Dimensions.get("screen").height / 7,
-    backgroundColor: "white",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    padding: 30,
-    alignItems: "center",
-    justifyContent: "space-between",
-    flexDirection: "row",
-  },
-
-  text: {
-    color: "black",
-    fontSize: 18,
-  },
-});
