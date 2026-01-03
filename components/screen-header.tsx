0865690892a94ccfa6d730fc4f10dbb0de03e581
Glenardd <tolentinojanglenard5@gmail.com>
2026-01-03 20:44:15 +0800

    updated the general header

--- components/screen-header.tsx
+++ components/screen-header.tsx
@@ -1,32 +1,38 @@
 import { useNavigation } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 type ScreenHeaderProps = {
-  title: string;
-  subtitle?: string;
+    title: string
+    subtitle?: string
+    currentPage?: string
 };
 
-export default function ScreenHeader({title, subtitle }: ScreenHeaderProps)  {
+export default function ScreenHeader({ title, subtitle, currentPage }: ScreenHeaderProps) {
     const navigation = useNavigation();
 
     return (
-        <View style={{ justifyContent: "flex-start" }}>
+        <View style={header_two.container}>
             <TouchableOpacity
                 onPress={() => navigation.goBack()}
                 style={{
-                    padding: 8,
-                    marginBottom: 5,
+                    paddingRight: 8,
+                    paddingTop: 8,
+                    paddingBottom: 8,
+                    marginBottom: 20,
                 }}
             >
-                <Text style={{
-                    color: "#63D0FF",
-                    fontSize: 14,
-                }}>
-                    Project Library
-                </Text>
+                <View style={{ flexDirection: "row", alignItems:"center" }}>
+                    <Image source={require("../assets/images/chevron_backward.png")} />
+                    <Text style={{
+                        color: "#63D0FF",
+                        fontSize: 14,
+                    }}>
+                        {currentPage}
+                    </Text>
+                </View>
             </TouchableOpacity>
-            <View style={header_two.container}>
+            <View>
                 <Text style={text.head}>{title}</Text>
                 <Text style={text.secondHead}>{subtitle}</Text>
             </View>
@@ -37,7 +43,7 @@ export default function ScreenHeader({title, subtitle }: ScreenHeaderProps)  {
 const header_two = StyleSheet.create({
     container: {
         paddingBottom: 15,
-        paddingHorizontal: 15
+        paddingHorizontal: 15,
     },
 
 })
