9450927cf16ca6ecb6beb922e98b8e973a9f0741
Glenardd <tolentinojanglenard5@gmail.com>
2026-01-26 22:27:24 +0800

    functions for searching, deleting, updating activities or projects

--- services/projects.service.ts
+++ services/projects.service.ts
@@ -1,37 +1,38 @@
 import { useOnRefreshByUser } from "@/hooks/useOnRefreshByUser"
 import { useRefreshOnFocus } from "@/hooks/useRefreshOnFocus"
 import { supabase } from "@/lib/supabase"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 interface ProjectTableType {
     isRefetchingByUser: boolean
     refetchByUser: () => void
-    isFetching: boolean
-    isPending: boolean
+    isLoading: boolean
     projects: ProjectObjectType[] | undefined
+    isPending: boolean
 }
 
-export interface ProjectObjectType{
+export interface ProjectObjectType {
     id: number
     created_at: string
     project_name: string
     user_id: string,
 }
 
-// view list of projects
-const useProjects = () : ProjectTableType =>{
+// list of all projects
+const useViewProjects = (): ProjectTableType => {
 
-    //fetch
-    const fetchProjects = async () : Promise<ProjectObjectType[]> => {
+    // fetch
+    const fetchProjects = async (): Promise<ProjectObjectType[]> => {
         const { data: projects } = await supabase.from('projects').select()
         return projects || []
     }
 
-    //query cache
-    const { data: projects, isFetching, dataUpdatedAt, refetch, isPending } = useQuery({
-        queryKey: ['projects'],
+    // query cache
+    const { data: projects, isFetching, dataUpdatedAt, refetch, isPending, isLoading } = useQuery({
+        queryKey: ["projects"],
         queryFn: fetchProjects,
-        staleTime: 1000 * 60 * 5,
+        staleTime: 1000 * 60 * 60 * 24,
+        refetchOnMount: "always"
     })
 
     // for refetching data
@@ -39,8 +40,72 @@ const useProjects = () : ProjectTableType =>{
     useRefreshOnFocus(refetch)
 
     // return cache
-    return { isRefetchingByUser, refetchByUser, isFetching, isPending, projects }
+    return { isRefetchingByUser, refetchByUser, isLoading, projects, isPending }
 }
 
-export { useProjects }
+//search project by id
+const useProjectById = (id: number) => {
+    const fetchProjects = async (): Promise<ProjectObjectType> => {
+        const { data: projects } = await supabase.from('projects').select().eq("id", id).single()
+        return projects
+    }
+
+    // query cache
+    const { data: searchProject } = useQuery({
+        queryKey: ["projects", id],
+        queryFn: fetchProjects,
+        staleTime: 1000 * 60 * 60 * 24,
+        refetchOnMount: "always"
+    })
+    
+    return { searchProject }
+}
+
+// insert data and mutate or update data
+const useInsertProject = () => {
+    const queryClient = useQueryClient()
+
+    const addProject = async (projectName: string) => {
+
+        // get the user
+        const {
+            data: { user },
+        } = await supabase.auth.getUser(); // user is an object
+
+        // json data payload
+        const payload = {
+            project_name: projectName,
+            user_id: user?.id
+        }
+
+        // insert data
+        const { data: projects, error } = await supabase
+            .from('projects')
+            .insert([payload]) // wrap in array
+            .select()
+
+        // console.log('Inserted project:', projects)
+
+        // error checking
+        if (error) throw error
+        return projects
+    }
+
+    // updates the data
+    const { mutate: updatedProject, isPending, isSuccess, error: error_mutation } = useMutation({
+        mutationFn: addProject,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["projects"],
+                exact: false
+            })
+        }
+    })
+
+    // error mutation checks
+    if (error_mutation) throw error_mutation
+    return { updatedProject, isPending }
+}
+
+export { useInsertProject, useProjectById, useViewProjects }
 
