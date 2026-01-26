d75dafe5fd336550b791b28162345cb1c70591c6
Glenardd <tolentinojanglenard5@gmail.com>
2026-01-26 22:18:54 +0800

    layout added for /dashboard/task/[project_id]/[activity_id]

---
+++ app/(app)/dashboard/task/[project_id]/[activity_id]/_layout.tsx
@@ -0,0 +1,35 @@
+import ScreenHeader from "@/components/screen-header";
+import { SplashScreenController } from "@/components/splash-screen-controller";
+import { useAuthContext } from "@/hooks/use-auth-context";
+import { Redirect, Stack } from "expo-router";
+export default function ActivityEditLayout() {
+
+    const { session, isLoading } = useAuthContext()
+
+    if (isLoading) {
+        <SplashScreenController />
+    }
+
+    if (!session) {
+        return <Redirect href="/login" />
+    }
+
+    return (
+        <Stack screenOptions={{ headerShown: false, animation: "none" }}>
+            <Stack.Screen name="index" options={{
+                headerShown: true,
+                contentStyle: { backgroundColor: "#070C27" },
+                headerTitle: () => <ScreenHeader title="Edit Activity" subtitle="Edit Activity" currentPage="Activity Input" editable={false} />,
+                headerStyle: {
+                    backgroundColor: "#070C27"
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+                headerBackVisible: false,
+                animation: "simple_push"
+            }} />
+        </Stack>
+    )
+}
