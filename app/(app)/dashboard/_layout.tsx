177fdc6ce3fed1a6654d06011e64c84e5d8ab70e
Glenardd <tolentinojanglenard5@gmail.com>
2026-01-26 22:11:28 +0800

    refactor dashboard layout

--- app/(app)/dashboard/_layout.tsx
+++ app/(app)/dashboard/_layout.tsx
@@ -1,7 +1,6 @@
 import { SplashScreenController } from "@/components/splash-screen-controller";
 import { useAuthContext } from "@/hooks/use-auth-context";
 import { Redirect, Stack } from "expo-router";
-import ProjectHeader from "./[project_id]";
 
 export default function DashboardLayout() {
 
@@ -16,22 +15,7 @@ export default function DashboardLayout() {
       }
 
     return (
-        <Stack screenOptions={{headerShown: false}}>
-            <Stack.Screen name="index" options={{
-                headerShown: true,
-                headerShadowVisible: false,
-                headerTitle: () => <ProjectHeader />,
-                headerStyle: {
-                    backgroundColor: "#070C27"
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                },
-                headerLeft: () => null,
-                headerBackVisible: false,
-                contentStyle: { backgroundColor: "#070C27" }
-            }} />
-        </Stack>
+        <Stack screenOptions={{headerShown: false}} />
+
     )
 }
