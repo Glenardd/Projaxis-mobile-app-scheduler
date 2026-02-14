b97092873a38031718fc8de108f2409d822aa81b
Glenardd <tolentinojanglenard5@gmail.com>
2026-02-15 03:40:30 +0800

    updated context for auth

--- hooks/use-auth-context.tsx
+++ hooks/use-auth-context.tsx
@@ -6,6 +6,7 @@ export type AuthData = {
   profile?: any | null
   isLoading: boolean
   isLoggedIn: boolean
+  logout: () => Promise<void>
 }
 
 export const AuthContext = createContext<AuthData>({
@@ -13,6 +14,7 @@ export const AuthContext = createContext<AuthData>({
   profile: undefined,
   isLoading: true,
   isLoggedIn: false,
+  logout: async () => {},
 })
 
 export const useAuthContext = () => useContext(AuthContext)
