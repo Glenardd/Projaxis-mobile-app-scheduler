8bc6d6a44c8fc089497f3769926a55478b6f1011
Glenardd <tolentinojanglenard5@gmail.com>
2026-03-04 12:31:19 +0800

    removed, updated

--- utils/reponsiveImageSize.ts
+++
@@ -1,6 +0,0 @@
-// for image responsiveness
-import { Dimensions } from "react-native";
-export const responsiveImageSize = (size: number) => {
-  const { width } = Dimensions.get("window");
-  return (width / 360) * size;
-};
--- utils/responsiveFontSize.ts
+++
@@ -1,4 +0,0 @@
-// for fontsize responsiveness
-import { Dimensions } from "react-native";
-const { width } = Dimensions.get("window");
-export const responsiveFont = (size: number) => (width / 360) * size;
